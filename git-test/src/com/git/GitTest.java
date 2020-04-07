ea00ffe94072d8c3a1e439fab5858746320762cb
chenbin <chenbin@qq.com>
2020-04-07 20:18:14 +0800

    加了个方法

--- git-test/src/com/git/GitTest.java
+++ git-test/src/com/git/GitTest.java
@@ -7,4 +7,9 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+
+    public void test(){
+        int a = 0 ;
+        System.out.println(a);
+    }
 }
